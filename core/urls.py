1039d86d3699eedf3a78be1efc585b2a5f51476b
DevAlone <dev@d3d.info>
2017-03-18 12:53:26 +0300

    url

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-fake_admin_regex = r'^admin/$|^wp-admin\.php$|^wp-login.php$|^administrator/$'
+fake_admin_regex = r'^admin/$|^wp-admin\.php$|^wp-login\.php$|^administrator/$'
 fake_admin_regex = r'|^admin.php$'
 fake_admin_regex += r'|^bitrix/admin/$|^manager/$'
 
