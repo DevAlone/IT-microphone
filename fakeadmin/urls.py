1c9fd519110b7eac5bfa3ad9758538238cee368c
DevAlone <dev@d3d.info>
2017-03-19 01:18:26 +0300

    fake admin

--- fakeadmin/urls.py
+++ fakeadmin/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 fake_admin_regex = r'^admin/$|^wp-admin\.php$|^wp-login\.php$|^administrator/$'
-fake_admin_regex += r'|^admin.php$'
-fake_admin_regex += r'|^bitrix/admin/$|^manager/$'
+fake_admin_regex += r'|^admin.php$|^login.php$|^cms/kernel/admin.php$'
+fake_admin_regex += r'|^bitrix/admin/$|^manager/$|^login_form$|^modx/manager/$'
+fake_admin_regex += r'|^administrator/index.php$'
+fake_admin_regex += r'|^this_page_is_not_exist_i_am_sure$'
 
 app_name = 'core'
 urlpatterns = [
