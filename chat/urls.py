a00420baa31c91686d758a4aa9bf17a09138fc65
DevAlone <dev@d3d.info>
2017-04-04 01:52:31 +0300

    m

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,8 @@ app_name = 'chat'
 urlpatterns = [
     url(r'^(?P<pk>[0-9]{1,10})/messages/$', views.getMessages,
         name='getMessages'),
+    url(r'^message/(?P<pk>[0-9]{1,10})/vote/(?P<action>[0-9])/$',
+        views.messageVote, name='messageVote'),
 #    url(r'^$', views.eventListView, {'category': 'future'}, name='index'),
 #    url(r'^event/(?P<pk>[0-9]{1,10})/$', views.eventDetail,
 #        name='event_detail'),
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from core.models import Event
 from django.forms.models import model_to_dict
-from .models import Chat
+from .models import Chat, ChatMessage
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.views.decorators.csrf import csrf_protect
 
 
 def getMessages(request, pk):
@@ -20,4 +23,42 @@ def getMessages(request, pk):
         'count_messages': chat.count_messages,
         'messages': messages,
     }
+    return JsonResponse(data)
+
+
+# this shit doesn't work
+@csrf_protect
+@login_required
+@transaction.atomic
+def messageVote(request, pk, action):
+    message = get_object_or_404(ChatMessage, pk=pk)
+    state = False
+    text = ""
+    print(pk)
+    print(action)
+    print()
+    if action == str(1):
+        if not message.likes.filter(pk=request.user.pk).exists():
+            message.dislikes.remove(request.user)
+            message.likes.add(request.user)
+            message.likes_count += 1
+            state = True
+        else:
+            text = 'Ты уже оценил комментарий'
+    else:
+        if not message.dislikes.filter(pk=request.user.pk).exists():
+            message.likes.remove(request.user)
+            message.dislikes.add(request.user)
+            message.likes_count += 1
+            state = True
+        else:
+            text = 'Ты уже оценил комментарий'
+
+    data = {
+        'state': state,
+        'text': text,
+        'likes_count': message.likes_count,
+        'dislikes_count': message.dislikes_count,
+    }
+
     return JsonResponse(data)
